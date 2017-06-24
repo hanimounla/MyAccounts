c2a394f71a56961f9b527bd54474588cfb4c2f7a
Hani Mounla <hani-mounla@live.com>
2017-06-24 09:09:09 +0300

    Cleand

--- app/src/main/java/com/example/hani__000/myaccounts/UpdateAccountFragment.java
+++ app/src/main/java/com/example/hani__000/myaccounts/UpdateAccountFragment.java
@@ -149,6 +149,5 @@ public class UpdateAccountFragment extends Fragment {
         FragmentManager fragMgr = getActivity().getSupportFragmentManager();
         getActivity().setTitle("My Accounts");
         fragMgr.popBackStack();
-
     }
 }
