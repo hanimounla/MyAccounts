1b57946320e9e6048c0bc23715c1236c6726a19b
Hani Mounla <hani-mounla@live.com>
2017-08-31 21:36:44 +0300

    added main setting and change password fragments

---
+++ app/src/main/java/com/example/hani__000/myaccounts/SettingsMainFragment.java
@@ -0,0 +1,13 @@
+package com.example.hani__000.myaccounts;
+
+import android.app.Fragment;
+
+/**
+ * Created by hani-_000 on 2017-08-31.
+ */
+
+public class SettingsMainFragment extends Fragment {
+
+
+
+}
